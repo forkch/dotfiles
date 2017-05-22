4a20c8b09e9f2213d51746a4380a528506c1474b
ben <ben@getbring.com>
2017-05-22 17:40:39 +0200

    updated for using fish

---
+++ config.fish
@@ -0,0 +1,21 @@
+set -Ux EDITOR vim
+
+set PATH $HOME/bin $PATH
+set PATH $HOME/Software/Android/sdk/tools $PATH
+set PATH $HOME/Software/Android/sdk/platform-tools $PATH
+set ANDROID_HOME $HOME/Software/Android/sdk
+
+for file in ~/.bash/*
+	. $file
+end
+
+
+abbr -a ga="git add"
+abbr -a gc="git commit"
+abbr -a gco="git checkout"
+abbr -a gst="git status"
+abbr -a gl="git pull"
+abbr -a gp="git push"
+abbr -a gu="git up"
+abbr -a s="git status -sb"
+abbr -a l="git log"
